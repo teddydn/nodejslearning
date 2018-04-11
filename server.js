4daf58003e120d958325fc161fc69cfda1790dd5
teddydn <teddydn@hotmail.com>
2018-04-11 11:23:25 +0800

    A simple HTTP server on local port 8888

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+http.createServer(function (request, response) {
+
+// send HTTP head 
+// HTTP status: 200 : OK
+// content type: text/plain
+response.writeHead(200, {'Content-Type': 'text/plain'});
+
+// send response data "Hello World"
+response.end('Hello World\n');
+}).listen(8888);
+
+// console print
+console.log('Server running at http://127.0.0.1:8888/');
